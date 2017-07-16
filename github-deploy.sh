8f3359453dcc930ebadd276c0299007567e60a7b
Pietro Bongiovanni <pietro@fixico.nl>
2017-07-16 22:32:08 +0200

    Updated github deploy to delete old branch before pushing

--- github-deploy.sh
+++ github-deploy.sh
@@ -6,4 +6,7 @@ then
       echo "Which folder do you want to deploy Ato GitHub Pages?"
         exit 1
     fi
+    echo "Deleting old website..."
+    git push origin --delete gh-pages
+    echo "Deploying new website..."
     git subtree push --prefix $1 origin gh-pages
