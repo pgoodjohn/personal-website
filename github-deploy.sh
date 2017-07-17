64c7313a2a5102ef04222daf70f0f4e271e72b10
Pietro Bongiovanni <pietro@fixico.nl>
2017-07-17 08:37:32 +0200

    Minify css before deploying

--- github-deploy.sh
+++ github-deploy.sh
@@ -6,6 +6,8 @@ then
       echo "Which folder do you want to deploy Ato GitHub Pages?"
         exit 1
     fi
+    echo "Minifying CSS..."
+    minify src/css/style.css --output src/css/style.min.css
     echo "Deleting old website..."
     git push origin --delete gh-pages
     echo "Deploying new website..."
