ecd159e0c55b616904edbfffb78d80da89d6fbae
Pietro Bongiovanni <pietro@fixico.nl>
2017-07-20 15:36:13 +0200

    Removed build from deployment because github doesn't like it

--- github-deploy.sh
+++ github-deploy.sh
@@ -4,8 +4,6 @@ then
       echo "Which folder do you want to deploy Ato GitHub Pages?"
         exit 1
     fi
-    echo "Minifying CSS..."
-    minify src/css/style.css --output src/css/style.min.css
     echo "Deleting old website..."
     git push origin --delete gh-pages
     echo "Deploying new website..."
